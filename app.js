60c91411ba5a736b16b723a00cc7db6753a9169a
MXD <MXD@MXD.local>
2016-08-24 11:37:42 -0600

    updated

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const hbs = require('hbs');
+const locals = require('./locals.js');
+
 let app = express();
 var port = 3000;
 
+app.set('view engine', 'hbs');
+hbs.localsAsTemplateData(app);
+
+locals(app);
+
 app.get('/', (req,res)=> {
-  res.json({message: 'Hello from Express!'});
+  res.render("home", {
+    // cohortName: 'g[31]',
+    foods: [
+      'Mexican',
+      'American',
+      'Italian'
+    ]
+  });
 });
 
 app.get('/page2', (req,res)=> {
-  res.json({message: 'I am page two now!'});
+  res.render("page2");
 });
 
 app.listen(port, () => {
---
+++ locals.js
@@ -0,0 +1,5 @@
+function locals (app) {
+  app.locals.cohortName = "g[31]";
+
+}
+module.exports = locals;
